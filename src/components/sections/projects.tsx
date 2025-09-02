709cedc3850d7d52ca1e7d8003e03af1eede7533
supriyakhanra <supriyakhanra3@gmail.com>
2025-09-03 00:00:27 +0530

    Update image alt text for accessibility

--- src/components/sections/projects.tsx
+++ src/components/sections/projects.tsx
@@ -18,7 +18,7 @@ const projectsData = [
   {
     title: "Portfolio Website",
     description: "A personal portfolio website built with Next.js and Tailwind CSS to showcase my skills, projects, and experience. Fully responsive and features a clean, modern design.",
-    image: "https://picsum.photos/600/400?random=2",
+    image: "https://images.unsplash.com/photo-1498050108023-c5249f4df085?q=80&w=1172&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     imageHint: "website portfolio",
     tags: ["Vibe Coding ","Next.js", "React", "Tailwind CSS", "TypeScript"],
     github: "https://github.com",
